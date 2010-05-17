1c1d11acd69212b5c58b245fcbcf041da61fc2a3
andreasknoepfle <andreas.knoepfle@googlemail.com>
2010-05-17 12:11:21 +0200

    Models Korrigiert

--- src/app/models/catalog.rb
+++ src/app/models/catalog.rb
@@ -1,7 +1,7 @@
 class Catalog < ActiveRecord::Base
   belongs_to :root_node , :class_name => "Node" , :foreign_key => "root_node_id"
   belongs_to :catalog_type
-  has_one :catalog_type
+  has_one :catalog_type ,  :foreign_key => "active_catalog_id"
   has_many :templates
 
 end
--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :appointments
   belongs_to :domain
-  has_many :tasks
+  has_many :tasks , :foreign_key => "creator_user_id"
   
 end
