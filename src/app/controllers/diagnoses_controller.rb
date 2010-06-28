f9a7a0f0ddf420be76b53e0e6a80a5263e372bec
fastridingzebra <fastridingzebra@gmail.com>
2010-06-28 11:23:09 +0200

    icd changes and diagnosis added

---
+++ src/app/controllers/diagnoses_controller.rb
@@ -0,0 +1,85 @@
+class DiagnosesController < ApplicationController
+  # GET /diagnoses
+  # GET /diagnoses.xml
+  def index
+    @diagnoses = Diagnosis.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @diagnoses }
+    end
+  end
+
+  # GET /diagnoses/1
+  # GET /diagnoses/1.xml
+  def show
+    @diagnosis = Diagnosis.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @diagnosis }
+    end
+  end
+
+  # GET /diagnoses/new
+  # GET /diagnoses/new.xml
+  def new
+    @diagnosis = Diagnosis.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @diagnosis }
+    end
+  end
+
+  # GET /diagnoses/1/edit
+  def edit
+    @diagnosis = Diagnosis.find(params[:id])
+  end
+
+  # POST /diagnoses
+  # POST /diagnoses.xml
+  def create
+    @diagnosis = Diagnosis.new(params[:diagnosis])
+
+    respond_to do |format|
+      if @diagnosis.save
+        flash[:notice] = 'Diagnosis was successfully created.'
+        format.html { redirect_to(@diagnosis) }
+        format.xml  { render :xml => @diagnosis, :status => :created, :location => @diagnosis }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @diagnosis.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /diagnoses/1
+  # PUT /diagnoses/1.xml
+  def update
+    @diagnosis = Diagnosis.find(params[:id])
+
+    respond_to do |format|
+      if @diagnosis.update_attributes(params[:diagnosis])
+        flash[:notice] = 'Diagnosis was successfully updated.'
+        format.html { redirect_to(@diagnosis) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @diagnosis.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /diagnoses/1
+  # DELETE /diagnoses/1.xml
+  def destroy
+    @diagnosis = Diagnosis.find(params[:id])
+    @diagnosis.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(diagnoses_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :diagnoses
+
  
   map.connect "/patients/search", :controller => "patients", :action => "search"
 
